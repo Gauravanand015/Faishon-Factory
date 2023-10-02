9a314fcf05d6577f0c96d67f9f7c73afbb42bfe0
Gaurav Anand <gauravanand67584@gmail.com>
2023-10-02 22:45:23 +0530

    functionality to the checkboxes

--- BackEnd/Router/productRoute.js
+++ BackEnd/Router/productRoute.js
@@ -1,11 +1,11 @@
-const  exp = require("express");
+const exp = require("express");
 const { CartModel } = require("../model/cartModel");
 const { ProductModel } = require("../model/productsModel");
 // const { upload } = require("./multer");
-const productRouter = exp.Router()
-const cloud = require("cloudinary").v2
+const productRouter = exp.Router();
+const cloud = require("cloudinary").v2;
 require("dotenv").config();
-const multer = require('multer');
+const multer = require("multer");
 
 // const storage = multer.diskStorage({
 //     destination: function (req, file, cb) {
@@ -15,29 +15,32 @@ const multer = require('multer');
 //       cb(null, Date.now() + '-' + file.originalname)
 //     }
 //   });
-  
-  const upload = multer({ dest: "uploads/" });
+
+const upload = multer({ dest: "uploads/" });
 
 cloud.config({
-    cloud_name: 'dcqylnqry',
-    api_key: '796765436163833',
-    api_secret: process.env.apiSecretKey,
-    secure: true
+  cloud_name: "dcqylnqry",
+  api_key: "796765436163833",
+  api_secret: process.env.apiSecretKey,
+  secure: true,
 });
 
-productRouter.get("/allData",async(req,res)=>{
-    try {
-        const allData = await ProductModel.find()
-        res.json(allData);
-    } catch (error) {
-        res.json(error)
-        console.log(error);
-    }
-})
+productRouter.get("/allData", async (req, res) => {
+  try {
+    const allData = await ProductModel.find();
+    res.json(allData);
+  } catch (error) {
+    res.json(error);
+    console.log(error);
+  }
+});
 
-productRouter.post("/create",upload.single("productImage"),async(req,res)=>{
-    console.log(req.body)
-    console.log(req.file)
+productRouter.post(
+  "/create",
+  upload.single("productImage"),
+  async (req, res) => {
+    console.log(req.body);
+    console.log(req.file);
     res.json("Hello");
     // console.log(file)
 
@@ -70,59 +73,71 @@ productRouter.post("/create",upload.single("productImage"),async(req,res)=>{
     //         console.log(error)
     //     }
     // })
-})
+  }
+);
 
 // productRouter.post("/create", async (req, res) => {
-    // console.log(req.body)
-    // res.send(req.body)
-    // try {
-        // const allData = await ProductModel.find();
-    //     const result = await cloud.uploader.upload(req.file.path);
-    //     const data = new ProductModel({
-    //         img: result.url,
-    //         title: req.body.title,
-    //         price: req.body.price,
-    //         delivery: req.body.delivery,
-    //         rating: req.body.rating,
-    //         reviews: req.body.reviews,
-    //         more: req.body.more,
-    //         product_id: allData.length + 1,
-    //     });
-    //     await data.save();
-    //     console.log(data);
-    //     res.json({ msg: "Created data" });
-    // } catch (error) {
-    //     console.log(error);
-    //     res.json("Something went wrong!!");
-    // }
+// console.log(req.body)
+// res.send(req.body)
+// try {
+// const allData = await ProductModel.find();
+//     const result = await cloud.uploader.upload(req.file.path);
+//     const data = new ProductModel({
+//         img: result.url,
+//         title: req.body.title,
+//         price: req.body.price,
+//         delivery: req.body.delivery,
+//         rating: req.body.rating,
+//         reviews: req.body.reviews,
+//         more: req.body.more,
+//         product_id: allData.length + 1,
+//     });
+//     await data.save();
+//     console.log(data);
+//     res.json({ msg: "Created data" });
+// } catch (error) {
+//     console.log(error);
+//     res.json("Something went wrong!!");
+// }
 // });
 
+productRouter.patch("/editnupdate/:product_id", async (req, res) => {
+  const id = req.params.product_id;
+  const data = req.body;
+  try {
+    const update = await ProductModel.updateOne({ product_id: id }, data);
+    res.json("Updated");
+    console.log(update);
+  } catch (error) {
+    res.json(error);
+  }
+});
 
-productRouter.patch("/editnupdate/:product_id",async (req,res)=>{
-    const id = req.params.product_id;
-    const data = req.body
-    try {
-        const update =  await ProductModel.updateOne({product_id:id},data)
-        res.json("Updated")
-        console.log(update);
-    } catch (error) {
-        res.json(error);
-    }
-})
+productRouter.delete("/delete/:product_id", async (req, res) => {
+  const id = req.params.product_id;
+  try {
+    const del = await ProductModel.deleteOne({ product_id: id });
+    await CartModel.deleteOne({ product_id: id });
+    res.json("Deleted");
+    console.log(del);
+  } catch (error) {
+    res.json(error);
+    console.log(error);
+  }
+});
 
-productRouter.delete("/delete/:product_id",async (req,res)=>{
-    const id = req.params.product_id;
-    try {
-        const del =  await ProductModel.deleteOne({product_id:id})
-        await CartModel.deleteOne({product_id:id})
-        res.json("Deleted")
-        console.log(del);
-    } catch (error) {
-        res.json(error);
-        console.log(error);
-    }
-})
+productRouter.get("/findBy/:type", async (req, res) => {
+  const type = req.params.type;
+  try {
+    const products = await ProductModel.find({
+      Title: { $regex: `\\b${type}\\b`, $options: "i" },
+    });
+    res.json(products);
+  } catch (err) {
+    res.status(500).json({ error: "Error fetching products" });
+  }
+});
 
-module.exports={
-    productRouter
-}
+module.exports = {
+  productRouter,
+};
--- FrontEnd/JS/index.js
+++ FrontEnd/JS/index.js
@@ -1,144 +1,146 @@
-let url = "https://good-cyan-goat-kilt.cyclic.app/home/getProduct"
+let url = "https://good-cyan-goat-kilt.cyclic.app/home/getProduct";
 let arr = [];
 
-let token =  sessionStorage.getItem("token");
-if(token){
-    document.querySelector(".SignUpHover").innerHTML = "Hi"+" "+ sessionStorage.getItem("userName")
+let token = sessionStorage.getItem("token");
+if (token) {
+  document.querySelector(".SignUpHover").innerHTML =
+    "Hi" + " " + sessionStorage.getItem("userName");
 }
 
 async function x() {
-    try {
-        let res = await fetch(url);
-        let data = await res.json();
-        arr = await data
-        // console.log(data)
-        renderData(data);
-    } catch (error) {
-        
-    }
+  try {
+    let res = await fetch(url);
+    let data = await res.json();
+    arr = await data;
+    // console.log(data)
+    renderData(data);
+  } catch (error) {}
 }
-x()
+x();
 
+function renderData(data) {
+  data.forEach((elem) => {
+    let id = elem.product_id;
 
-function renderData(data){
-    console.log(data)
-    data.forEach(elem => {
+    let sub_div = document.createElement("div");
+    sub_div.setAttribute("class", "sub");
 
-        let id = elem.product_id
-
-        let sub_div = document.createElement("div");
-        sub_div.setAttribute("class","sub");
-
-        sub_div.addEventListener("click",()=>{
-            pro_des(id)
-        })
-
-        let image_div = document.createElement("div")
-        image_div.setAttribute("class","img")
-
-        let image = document.createElement("img");
-        image.setAttribute("src",elem.img)
-
-        image_div.append(image)
-
-        let pro_details = document.createElement("div")
-        pro_details.setAttribute("class","pro_details")
-
-        let title = document.createElement("div");
-        title.setAttribute("class","title")
-        title.innerText = elem.Title && elem.Title.length > 20 ? elem.Title.substring(0, 20) + "..." : elem.Title;
-
-        let price = document.createElement("div");
-        price.setAttribute("class","price")
-        price.innerText=elem.price;
-
-        let delivery = document.createElement("div");
-        delivery.setAttribute("class","delivery")
-        delivery.innerText=elem.Delivery;
-
-        let rating = document.createElement("div");
-        rating.setAttribute("class","rating")
-        rating.innerText=elem.Rating;
-
-        pro_details.append(title,price,delivery,rating)
-
-        sub_div.append(image_div,pro_details)
-        document.querySelector(".card-list").append(sub_div)
+    sub_div.addEventListener("click", () => {
+      pro_des(id);
     });
+
+    let image_div = document.createElement("div");
+    image_div.setAttribute("class", "img");
+
+    let image = document.createElement("img");
+    image.setAttribute("src", elem.img);
+
+    image_div.append(image);
+
+    let pro_details = document.createElement("div");
+    pro_details.setAttribute("class", "pro_details");
+
+    let title = document.createElement("div");
+    title.setAttribute("class", "title");
+    title.innerText =
+      elem.Title && elem.Title.length > 20
+        ? elem.Title.substring(0, 20) + "..."
+        : elem.Title;
+
+    let price = document.createElement("div");
+    price.setAttribute("class", "price");
+    price.innerText = elem.price;
+
+    let delivery = document.createElement("div");
+    delivery.setAttribute("class", "delivery");
+    delivery.innerText = elem.Delivery;
+
+    let rating = document.createElement("div");
+    rating.setAttribute("class", "rating");
+    rating.innerText = elem.Rating;
+
+    pro_details.append(title, price, delivery, rating);
+
+    sub_div.append(image_div, pro_details);
+    document.querySelector(".card-list").append(sub_div);
+  });
 }
 
-async function pro_des(id){
-    try {
-        let res = await fetch(`https://good-cyan-goat-kilt.cyclic.app/productDescription/product/${id}`)
-        let data  = await res.json()
-        console.log(data)
-        localStorage.setItem("pro_des",JSON.stringify(data))
-         window.location.href="product_description.html"
-    } catch (error) {
-        console.log(error)
-    }
+async function pro_des(id) {
+  try {
+    let res = await fetch(
+      `https://good-cyan-goat-kilt.cyclic.app/productDescription/product/${id}`
+    );
+    let data = await res.json();
+    console.log(data);
+    localStorage.setItem("pro_des", JSON.stringify(data));
+    window.location.href = "product_description.html";
+  } catch (error) {
+    console.log(error);
+  }
 }
-
-
-// function search(){
-//     console.log(arr)
-//     let ser = document.getElementById("search").value;
-//     let newData = arr.filter(function(elem){
-//         return elem.Title.toLowerCase().includes(ser.toLocaleLowerCase())
-//     });
-//     renderData(newData)
-// }
-
 
 function search() {
-    let ser = document.getElementById("search").value;
-    let newData = arr.filter(function(elem) {
-      return elem.Title && elem.Title.toLowerCase().includes(ser.toLowerCase());
-    });
-    document.querySelector(".card-list").innerHTML = ""; // Clear existing results
-    renderData(newData); // Display filtered results
-  }
-
-
-// function sort() {
-//     let sel = document.getElementById("sort").value;
-//     console.log(sel);
-//     if (sel == "LTH") {
-//       arr.sort((a, b) => a.price.slice(1) - b.price.slice(1));
-//       document.querySelector(".card-list").innerHTML = ""
-//       renderData(arr);
-//     }
-//     if (sel == "HTL") {
-//       arr.sort((a, b) => b.price.slice(1) - a.price.slice(1));
-//       document.querySelector(".card-list").innerHTML = ""
-//       renderData(arr);
-//     }
-//   }
+  let ser = document.getElementById("search").value;
+  let newData = arr.filter(function (elem) {
+    return elem.Title && elem.Title.toLowerCase().includes(ser.toLowerCase());
+  });
+  document.querySelector(".card-list").innerHTML = ""; // Clear existing results
+  renderData(newData); // Display filtered results
+}
 
 function sort() {
-    let sel = document.getElementById("sort").value;
-    console.log(sel);
-    if (sel == "LTH") {
-      arr.sort((a, b) => {
-        if (a.price && b.price) {
-          return a.price.slice(1) - b.price.slice(1);
-        } else {
-          return 0;
-        }
-      });
-      document.querySelector(".card-list").innerHTML = "";
-      renderData(arr);
-    }
-    if (sel == "HTL") {
-      arr.sort((a, b) => {
-        if (a.price && b.price) {
-          return b.price.slice(1) - a.price.slice(1);
-        } else {
-          return 0;
-        }
-      });
-      document.querySelector(".card-list").innerHTML = "";
-      renderData(arr);
-    }
+  let sel = document.getElementById("sort").value;
+  console.log(sel);
+  if (sel == "LTH") {
+    arr.sort((a, b) => {
+      if (a.price && b.price) {
+        return a.price.slice(1) - b.price.slice(1);
+      } else {
+        return 0;
+      }
+    });
+    document.querySelector(".card-list").innerHTML = "";
+    renderData(arr);
   }
+  if (sel == "HTL") {
+    arr.sort((a, b) => {
+      if (a.price && b.price) {
+        return b.price.slice(1) - a.price.slice(1);
+      } else {
+        return 0;
+      }
+    });
+    document.querySelector(".card-list").innerHTML = "";
+    renderData(arr);
+  }
+}
 
+function getCheckedCheckboxes() {
+  const checkboxes = document.querySelectorAll(".xyz");
+  for (let checkbox of checkboxes) {
+    checkbox.addEventListener("click", (event) => {
+      if (event.target.checked) {
+        getValuesOfCheckbox(event.target.name.toLowerCase());
+      } else {
+        document.querySelector(".card-list").innerHTML = "";
+        renderData(arr);
+      }
+    });
+  }
+}
+
+async function getValuesOfCheckbox(value) {
+  try {
+    let res = await fetch(
+      `https://good-cyan-goat-kilt.cyclic.app/product/findBy/${value}`
+    );
+    let data = await res.json();
+    document.querySelector(".card-list").innerHTML = "";
+    renderData(data);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+getCheckedCheckboxes();
